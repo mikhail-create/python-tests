ebfeeae94cf279b986f1cc357688414dc4bee423
mikhail-create <leonovm143@gmail.com>
2021-11-11 16:10:23 +0600

    added SC parser

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# python-tests
+# Automation with Python
+
+## putty.py
+
+ Реалзация управления графической утилитой putty в части подключения к сервисам по протоколу ssh
+
+## parser.py
+
+ Парсинг сайта IOT VEGA сервер с выгрузкой всех доступных устройств в excel c помощью Selenium.
+
+## parserSC.py
+
+ Загрузчик музыки с SoundCloud
---
+++ parserSC.py
@@ -0,0 +1,17 @@
+import time
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+url = 'https://sclouddownloader.net/'
+music = 'https://soundcloud.com/dioresly/twinky-ghoul'
+driver.get(url)
+time.sleep(2)
+inputarea = driver.find_element_by_css_selector('.input-group-field')
+inputarea.send_keys(music)
+driver.find_element_by_css_selector('.button').click()
+time.sleep(2)
+driver.find_element_by_xpath("//a[text()='Download Track ']").click()
+time.sleep(5)
+driver.close()
