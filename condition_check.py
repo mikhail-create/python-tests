a846c370910de800199208df2f7e944bd5eb2fe3
mikhail-create <leonovm143@gmail.com>
2021-11-17 11:13:57 +0600

    added LR5

---
+++ condition_check.py
@@ -0,0 +1,32 @@
+import psutil
+import time
+
+disk = psutil.disk_partitions()
+i=0
+stat_file = open("stats.dat", "a")
+
+stat_file.write(f"@Disk @RAM\n")
+
+def file(i, disk, ram):
+    stat_file.write(f"{i}\t{disk}\t{ram}\n")
+
+
+while i<10:
+    disk_C = psutil.disk_usage (disk [0] [0])
+    ram = psutil.virtual_memory().available * 100 / psutil.virtual_memory().total
+
+    file(i, disk_C.percent, ram)
+
+    print(i)
+    print ('Процент использования диска C: ', disk_C.percent)
+    print ('Процент использования оперативной памяти: ', ram)
+    print('==============')
+
+    if (ram > 80):
+        print('Высокая нагрузка на оперативную память')
+    i = i+1
+    time.sleep(1)
+
+print("END")
+
+#termgraph stats.dat --color {yellow,magenta}
